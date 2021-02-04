9c6e2c1619d90327965fb3f2ac7d9006b96507d5
Pablo Carreño <carp4392@gmail.com>
2021-02-03 21:25:58 -0600

    nuevo commit

--- javascript/api.js
+++ javascript/api.js
@@ -30,10 +30,10 @@ function printData(data) {
     questionContainer.innerHTML = "";
 
     data.results.forEach((element, index) => {
-        const answer01 = `<label class="form-check-label" for="correct"><input class="form-check-input" type="radio" name="card-${index}" id="correct" value="1" required>${element.correct_answer}</label>`,
-              answer02 = `<label class="form-check-label" for="wrong01"><input class="form-check-input" type="radio" name="card-${index}" id="wrong01" value="0" required>${element.incorrect_answers[0]}</label>`,
-              answer03 = `<label class="form-check-label" for="wrong02"><input class="form-check-input" type="radio" name="card-${index}" id="wrong02" value="0" required>${element.incorrect_answers[1]}</label>`,
-              answer04 = `<label class="form-check-label" for="wrong03"><input class="form-check-input" type="radio" name="card-${index}" id="wrong03" value="0" required>${element.incorrect_answers[2]}</label>`;
+        const answer01 = `<label class="form-check-label"><input class="form-check-input" type="radio" name="card-${index}" id="correct" value="1" required>${element.correct_answer}</label>`,
+              answer02 = `<label class="form-check-label"><input class="form-check-input" type="radio" name="card-${index}" id="wrong01" value="0" required>${element.incorrect_answers[0]}</label>`,
+              answer03 = `<label class="form-check-label"><input class="form-check-input" type="radio" name="card-${index}" id="wrong02" value="0" required>${element.incorrect_answers[1]}</label>`,
+              answer04 = `<label class="form-check-label"><input class="form-check-input" type="radio" name="card-${index}" id="wrong03" value="0" required>${element.incorrect_answers[2]}</label>`;
         const arrayAnswers = [answer01, answer02, answer03, answer04];
 
         //Random function
@@ -41,7 +41,7 @@ function printData(data) {
             return Math.random() - 0.5
         })
         
-        questionContainer.innerHTML += `<div class="col-md-3 my-4">
+        questionContainer.innerHTML += `<div class="col-md-6 my-4">
                                             <div class="card h-100 text-color background-question">
                                                 <div class="card-body">
                                                     <div>${element.question}</div>
@@ -50,7 +50,8 @@ function printData(data) {
                                                             <p> ${arrayAnswers[0]} </p>
                                                             <p> ${arrayAnswers[1]} </p>
                                                             <p> ${arrayAnswers[2]} </p>
-                                                            <p> ${arrayAnswers[3]} </p>                                                
+                                                            <p> ${arrayAnswers[3]} </p>
+                                                            <button> Score </button>
                                                         </form>                      
                                                     </div>
                                                 </div>
@@ -59,20 +60,16 @@ function printData(data) {
     });
 }
 
-// function results() {
-//     const selectResul = document.querySelector('input[name="card-0"]:checked').value;
-//     const selectResul = document.querySelector('input[name="card-1"]:checked').value;
-//     const selectResul = document.querySelector('input[name="card-2"]:checked').value;
-//     const selectResul = document.querySelector('input[name="card-3"]:checked').value;
+function results() {
+    const array = [0, 1, 1, 1, 0, 0, 0, 1, 0];
+    const score = 0;
 
+    array.forEach(element => {
+        score += element
+    })
 
-//     console.log(selectResul);
-//     if(selectResul == 1 ) {
-//         alert('Tu respuesta es correcta');
-//     } else {
-//         alert('Tu respuesta es incorrecta');
-//     }
-// }
+    console.log(ejecutando)
+}
 
 getCategories();
 
